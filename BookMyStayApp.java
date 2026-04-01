f11d65b7839ddb59ee508079277d35415d21da72
nm5758 <nm5758@srmist.edu.in>
2026-04-01 13:47:38 +0530

    UC2

--- BookMyStayApp.java
+++ BookMyStayApp.java
@@ -1,3 +1,26 @@
 public class BookMyStayApp{
     
+
+    void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq ft");
+        System.out.println("Price per Night: ₹" + price);
+    }
 }
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 200, 2500);
+    }
+}
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 350, 4000);
+    }
+}
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 600, 8000);
+    }
+}
+   
