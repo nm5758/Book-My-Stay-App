9c5eb9fb7b6f409da17ae787df26da38b51ff6ef
nm5758 <nm5758@srmist.edu.in>
2026-04-01 14:30:02 +0530

    UC3

--- BookMyStayApp.java
+++ BookMyStayApp.java
@@ -1,31 +1,33 @@
-public class BookMyStayApp{
+import java.util.HashMap;
+import java.util.Map;
+class RoomInventory {
 
-    public static void main(String[] args){
-        System.out.println("welcome to book my stay app");
-        
+    private Map<String, Integer> inventory;
+    RoomInventory() {
+        inventory = new HashMap<>();
     }
-    
+    void addRoomType(String type, int count) {
+        inventory.put(type, count);
+    }
+    int getAvailability(String type) {
+        return inventory.getOrDefault(type, 0);
+    }
+    void updateAvailability(String type, int change) {
+        int current = inventory.getOrDefault(type, 0);
 
-    void displayRoomDetails() {
-        System.out.println("Room Type: " + roomType);
-        System.out.println("Beds: " + beds);
-        System.out.println("Size: " + size + " sq ft");
-        System.out.println("Price per Night: ₹" + price);
+        if (current + change < 0) {
+            System.out.println("Not enough rooms available for " + type);
+            return;
+        }
+
+        inventory.put(type, current + change);
+    }
+    void displayInventory() {
+        System.out.println("\nCurrent Room Inventory:");
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
     }
 }
-class SingleRoom extends Room {
-    SingleRoom() {
-        super("Single Room", 1, 200, 2500);
-    }
-}
-class DoubleRoom extends Room {
-    DoubleRoom() {
-        super("Double Room", 2, 350, 4000);
-    }
-}
-class SuiteRoom extends Room {
-    SuiteRoom() {
-        super("Suite Room", 3, 600, 8000);
-    }
-}
-   
+
+
